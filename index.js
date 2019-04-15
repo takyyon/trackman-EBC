0696af93ad5333188f40ede1ce2c81e6f54c8035
Oded Dvoskin <odvoskin@microsoft.com>
2019-04-15 15:47:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Staging, I want to be prod!");
+    response.end("Hello World - I am Staging!");
 
 });
 
